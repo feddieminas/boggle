37af301f2cbe767de265716aa36366735ff91a17
Faidon Dermesonoglou <faidon.dermesonoglou@gmail.com>
2019-01-08 12:51:45 +0000

    words

--- test_boggle.py
+++ test_boggle.py
@@ -1,6 +1,6 @@
 import unittest #uses classes and inheritance
 import boggle
-
+from string import ascii_uppercase
 
 class TestBoggle(unittest.TestCase):
     """
@@ -32,4 +32,81 @@ class TestBoggle(unittest.TestCase):
         self.assertIn((0,1), grid)
         self.assertIn((1,0), grid)
         self.assertIn((1,1), grid)        
-        self.assertNotIn((2,2), grid) #assert that 2,2 is not in a 2*2 grid        
+        self.assertNotIn((2,2), grid) #assert that 2,2 is not in a 2*2 grid 
+        
+    
+    def test_grid_is_filled_with_letters(self):
+        """
+        Ensure that each of the coordinates in the grid
+        contains letters
+        """
+        grid = boggle.make_grid(2,3)
+        for letter in grid.values():
+            self.assertIn(letter, ascii_uppercase)
+            
+    def test_neighbours_of_a_position(self):
+        """
+        Ensure that a position has 8 neighbours
+        """
+        coords = (1,2)
+        neighbours = boggle.neighbours_of_position(coords)
+        self.assertIn((0,1), neighbours)
+        self.assertIn((0,2), neighbours)
+        self.assertIn((0,3), neighbours)
+        self.assertIn((1,1), neighbours)
+        self.assertIn((1,3), neighbours)   
+        self.assertIn((2,1), neighbours)
+        self.assertIn((2,2), neighbours)  
+        self.assertIn((2,3), neighbours)        
+        
+    def test_all_grid_neighbours(self):
+        """
+        Ensure that all of the grid have neighbours
+        """
+        grid = boggle.make_grid(2,2)
+        neighbours = boggle.all_grid_neighbours(grid) #get all neighbours of a grid
+        self.assertEqual(len(neighbours), len(grid)) #assert the correct length of neighbours dict
+        for pos in grid: #iterate through the positions in the grid for each position
+            others = list(grid)
+            others.remove(pos) # thus we create the full grid but remove the pos to show the neighbours only
+            self.assertListEqual(sorted(neighbours[pos]), sorted(others)) #the positions of the neighbours are the positions being checked
+            #it gives us the confidence that our neighbours data structure is correct    
+        
+    def test_converting_a_path_to_a_word(self): # turn a path/string of positions into a word/list of letters
+        """
+        Ensure that paths can be converted to words
+        """
+        grid = boggle.make_grid(2,2)
+        oneLetterWord = boggle.path_to_word(grid, [(0,0)])
+        twoLetterWord = boggle.path_to_word(grid, [(0,0),(1,1)])
+        self.assertEqual(oneLetterWord, grid[(0,0)])
+        self.assertEqual(twoLetterWord, grid[(0,0)] + grid[(1,1)])        
+        
+    def test_search_grid_for_words(self): # need to find words in the grid. need to ensure that certain patterns can be found in a path    
+        """
+        Ensure that certain patterns can be found in a path_to_word
+        """
+        grid = {(0,0): 'A',(0,1): 'B',(1,0): 'C',(1,1): 'D'}
+        twoLetterWord = 'AB'
+        threeLetterWord = 'ABC'
+        notThereWord = 'EEE'
+        dictionary = [twoLetterWord, threeLetterWord, notThereWord]
+        
+        foundWords = boggle.search(grid, dictionary)
+        
+        self.assertTrue(twoLetterWord in foundWords)
+        self.assertTrue(threeLetterWord in foundWords)
+        self.assertTrue(notThereWord not in foundWords)        
+        
+    def test_load_dictionary(self):
+        """
+        Test that the 'get_dictionary' function returns a dictionary
+        that has a length greater than 0
+        """
+        dictionary = boggle.get_dictionary('words.txt')
+        self.assertGreater(len(dictionary), 0)
+        
+        
+        
+        
+        
